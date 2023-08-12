3d7777ffb091f39e24b8081785af52f0f9385c8b
yyland <adseain-nebamu8@yahoo.co.jp>
2023-08-12 23:41:48 +0900

    rubocop指摘箇所修正

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :english_texts;
+  has_many :english_texts, dependent: :destroy
 
-  def email=(value)
-    self[:email] = self[:username] + "@eibunmemo.com"
+  def email=(_value)
+    self[:email] = "#{self[:username]}@eibunmemo.com"
   end
 
   validates :username, presence: true, uniqueness: true
