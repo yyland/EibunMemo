dcae84412dad76fc863b0e3ea3c1636eb7a5ec73
yyland <adseain-nebamu8@yahoo.co.jp>
2023-08-17 17:32:41 +0900

    ユーザーメニューをコンポーネント化
    UserProfileMenu追加。

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,26 +1,19 @@
 import EnglishTextList from "./EnglishTextList";
-import { VStack, Box, Button, Menu as ChakraMenu, MenuButton, MenuList, MenuItem, Text } from "@chakra-ui/react";
+import { VStack, Box } from "@chakra-ui/react";
 import { NewTextButton } from '../components/MenuButton';
-import { signOut } from '../lib/api/auth.js';
-import { useNavigate } from 'react-router-dom';
-import { Avatar } from "@chakra-ui/avatar";
 import { UnauthenticatedHeader } from "./UnauthenticatedHeader";
+import { UserProfileMenu } from "./UserProfileMenu";
 
-const Menu = ({setEnglishTexts, selectedText, setSelectedComponent, setSelectedText, englishTexts, isLoggedIn, setIsLoggedIn, userName }) => {
-
-  const navigate = useNavigate();
-
-  const logOut = async () => {
-    try {
-      const res = await signOut();
-      if (res) {
-        setIsLoggedIn(false);
-        navigate('/');
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
+const Menu = ({
+  setEnglishTexts, 
+  selectedText, 
+  setSelectedComponent, 
+  setSelectedText, 
+  englishTexts, 
+  isLoggedIn, 
+  setIsLoggedIn, 
+  userName 
+}) => {
 
   return (
     <VStack 
@@ -52,60 +45,19 @@ const Menu = ({setEnglishTexts, selectedText, setSelectedComponent, setSelectedT
               setSelectedComponent={setSelectedComponent}
             />
           </Box>
+          <Box 
+            position="fixed"
+            bottom="0"  // VStack の下部に固定
+            w="full"
+            bgColor="blue.800"
+            py={2}
+            borderTop="2px solid"
+            borderColor="whiteAlpha.100"
+          >
+            <UserProfileMenu setIsLoggedIn={setIsLoggedIn} userName={userName} />
+          </Box>
         </>
       )}
-
-      {isLoggedIn && (
-        <Box 
-          position="fixed"
-          bottom="0"  // VStack の下部に固定
-          w="full"
-          bgColor="blue.800"
-          py={2}
-          borderTop="2px solid"
-          borderColor="whiteAlpha.100"
-        >
-          <ChakraMenu>
-            <MenuButton
-              as={Button}
-              leftIcon={
-                <Avatar 
-                  size="xs" 
-                  name={userName} 
-                  marginLeft={"-0.3em"}
-                  color={"whiteAlpha.900"}
-                  bgColor="blue.600"
-                />
-              }
-              pr={6}
-              iconSpacing="0.6em"
-              color="white"
-              bg="transparent"
-              _hover={{ bg: "rgba(255, 255, 255, 0.1)" }}
-              _active={{ bg: "transparent" }}
-            >
-              <Text fontSize="md" fontWeight="normal" lineHeight="1.2em" letterSpacing="0.05em">
-                {userName}
-              </Text>
-            </MenuButton>
-            <MenuList bg="blue.900" borderColor="gray.600" borderWidth="1px" padding={0}>
-              <MenuItem
-                color="white"
-                bg="transparent"
-                py={3}
-                pl={9}
-                _hover={{ bg: "rgba(255, 255, 255, 0.1)"}}
-                _expanded={{ bg: "transparent" }}
-                onClick={logOut}
-              >
-                Log Out
-              </MenuItem>
-            </MenuList>
-          </ChakraMenu>
-        </Box>
-      )}
-
-
     </VStack>
   );
 };
---
+++ frontend/src/components/UserProfileMenu.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  Menu as ChakraMenu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Text,
+  Button,
+  Avatar,
+} from "@chakra-ui/react";
+import { signOut } from '../lib/api/auth.js';
+import { useNavigate } from 'react-router-dom';
+
+export const UserProfileMenu = ({ setIsLoggedIn, userName }) => {
+
+  const navigate = useNavigate();
+
+  const logOut = async () => {
+    try {
+      const res = await signOut();
+      if (res) {
+        setIsLoggedIn(false);
+        navigate('/');
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <ChakraMenu>
+      <MenuButton
+        as={Button}
+        leftIcon={
+          <Avatar 
+            size="xs" 
+            name={userName} 
+            marginLeft={"-0.3em"}
+            color={"whiteAlpha.900"}
+            bgColor="blue.600"
+          />
+        }
+        pr={6}
+        iconSpacing="0.6em"
+        color="white"
+        bg="transparent"
+        _hover={{ bg: "rgba(255, 255, 255, 0.1)" }}
+        _active={{ bg: "transparent" }}
+      >
+        <Text fontSize="md" fontWeight="normal" lineHeight="1.2em" letterSpacing="0.05em">
+          {userName}
+        </Text>
+      </MenuButton>
+      <MenuList bg="blue.900" borderColor="gray.600" borderWidth="1px" padding={0}>
+        <MenuItem
+          color="white"
+          bg="transparent"
+          py={3}
+          pl={9}
+          _hover={{ bg: "rgba(255, 255, 255, 0.1)"}}
+          _expanded={{ bg: "transparent" }}
+          onClick={logOut}
+        >
+          Log Out
+        </MenuItem>
+      </MenuList>
+    </ChakraMenu>
+  )
+}
